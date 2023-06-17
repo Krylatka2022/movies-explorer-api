8e92b13db3df2bf34ba17194121c0a4469399fa9
Daria Krushelnitskaya <dasha_fr_russia@mail.ru>
2023-06-17 11:50:18 +0300

    controllers updateUser

--- controllers/users.js
+++ controllers/users.js
@@ -58,6 +58,8 @@ const updateUser = (req, res, next) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные при обновлении профиля'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким email уже зарегистрирован'));
       } else {
         next(err);
       }
